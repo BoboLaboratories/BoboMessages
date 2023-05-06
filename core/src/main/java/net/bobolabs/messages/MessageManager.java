4198208c21a2bd5fa8bc8b7c591cc1745a3dcc31
Fabio Nebbia <fabionebbia97@gmail.com>
2023-05-06 15:01:07 +0200

    funziona ma non so perche', sono nabbo, mvb

--- core/src/main/java/net/bobolabs/messages/MessageManager.java
+++ core/src/main/java/net/bobolabs/messages/MessageManager.java
@@ -1,5 +1,6 @@
 package net.bobolabs.messages;
 
+import net.bobolabs.core.Reloadable;
 import org.jetbrains.annotations.NotNull;
 
 public interface MessageManager<T, U extends AbstractMessage<T>> extends Reloadable {
